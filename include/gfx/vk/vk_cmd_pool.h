e7aeaa6babd94312dc39a4ff54c7c78c64161dd6
Chillzyy <94079092+Chillzyy@users.noreply.github.com>
2024-07-24 10:08:58 -0400

    Update vk_cmd_pool.h

--- include/gfx/vk/vk_cmd_pool.h
+++ include/gfx/vk/vk_cmd_pool.h
@@ -2,7 +2,7 @@
 #define vk_cmd_pool_h
 
 #include <stdio.h>
-#include "vulkan/vulkan.h"
+#include <vulkan/vulkan.h>
 
 VkCommandPool vk_cmd_pool_init(VkDevice device, VkCommandPoolCreateFlags flags, uint32_t queue_family_index);
 void vk_cmd_pool_destroy(VkDevice device, VkCommandPool cmd_pool);
