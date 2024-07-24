28a26a2fad80001ff497497594ec68610d7e3a51
Chillzyy <94079092+Chillzyy@users.noreply.github.com>
2024-07-24 10:10:30 -0400

    Update vk_instance.h

--- include/gfx/vk/vk_instance.h
+++ include/gfx/vk/vk_instance.h
@@ -1,12 +1,12 @@
 #ifndef vk_instance_h
 #define vk_instance_h
 
-#include "vulkan/vulkan.h"
-#include "GLFW/glfw3.h"
-#include "gfx/vk/vk_ext.h"
-#include "gfx/vk/vk_debug.h"
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "gfx/vk/vk_ext.h"
+#include "gfx/vk/vk_debug.h"
 
 VkInstance vk_instance_init(const char *title, bool debugging);
 void vk_instance_destroy(VkInstance instance);
