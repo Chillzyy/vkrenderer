9097e1ca4afa0f2f460417ed67efba47d19bc081
Chillzyy <94079092+Chillzyy@users.noreply.github.com>
2024-07-24 10:11:58 -0400

    Update vk_surface.h

--- include/gfx/vk/vk_surface.h
+++ include/gfx/vk/vk_surface.h
@@ -2,8 +2,8 @@
 #define vk_surface_h
 
 #include <stdio.h>
-#include "vulkan/vulkan.h"
-#include "GLFW/glfw3.h"
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
 
 VkSurfaceKHR vk_surface_init(VkInstance instance, GLFWwindow *window);
 void vk_surface_destroy(VkInstance instance, VkSurfaceKHR surface);
