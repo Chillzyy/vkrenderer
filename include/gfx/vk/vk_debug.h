f2af03bbf71ead14f90daac76d7780253eb32868
Chillzyy <94079092+Chillzyy@users.noreply.github.com>
2024-07-25 09:11:03 -0400

    Update vk_debug.h

--- include/gfx/vk/vk_debug.h
+++ include/gfx/vk/vk_debug.h
@@ -6,7 +6,7 @@
 #include <vulkan/vulkan.h>
 
 VkDebugUtilsMessengerEXT vk_debug_messenger_init(VkInstance instance);
-void vk_debug_messenger_create_info(VkDebugUtilsMessengerCreateInfoEXT *debug_info);
+void vk_debug_messenger_create_info_init(VkDebugUtilsMessengerCreateInfoEXT *debug_info);
 VKAPI_ATTR VkResult VKAPI_CALL vk_debug_messenger_create(VkInstance instance, VkDebugUtilsMessengerEXT *debug_messenger, const VkDebugUtilsMessengerCreateInfoEXT *debug_info, const VkAllocationCallbacks *alloc);
 VKAPI_ATTR void VKAPI_CALL vk_debug_messenger_destroy(VkInstance instance, VkDebugUtilsMessengerEXT debug_messenger, const VkAllocationCallbacks *alloc);
 
